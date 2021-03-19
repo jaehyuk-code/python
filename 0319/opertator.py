d4ea6470a580acc5ed7d2a56ef2898b586219bc6
jaehyuk-code <80086101+jaehyuk-code@users.noreply.github.com>
2021-03-19 16:05:13 +0900

    Add files via upload

---
+++ 0319/opertator.py
@@ -0,0 +1,18 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/Mac_1/Documents/python재혁/0319/opertator.py ====
+3 -4
+>>> 
+===== RESTART: C:/Users/Mac_1/Documents/python재혁/0319/opertator.py ====
+3 -4
+3 5 1
+3 5 6
+3 5 7
+>>> 
+===== RESTART: C:/Users/Mac_1/Documents/python재혁/0319/opertator.py ====
+a= 3 b= -4
+a= 3 b= 5 c= 1
+a= 3 b= 5 c= 6
+a= 3 b= 5 c= 7
+>>> 
