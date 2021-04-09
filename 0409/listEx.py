42a08d767a655307036c6b116a14f81149051519
jaehyuk-code <80086101+jaehyuk-code@users.noreply.github.com>
2021-04-09 15:48:19 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,11 @@
+fstName = ['J', 'a', 'n', 'g']
+sndName = ["Jae" , "Hyuk"]
+stdNum = [2 , 0, 2, 1, 0, 6, 0, 0, 1]
+
+print(fstName)
+print(sndName)
+# 화면에 J, J, H를 출력하는 프로그램을 작성하시오.
+print(fstName[0])     #J
+                      #J
+                      #H
+ 
