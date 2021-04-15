1e27132a0664600d025d09b76b45b989984777df
jaehyuk-code <80086101+jaehyuk-code@users.noreply.github.com>
2021-04-15 16:57:28 +0900

    Add files via upload

---
+++ 0415/ifStmt.py
@@ -0,0 +1,13 @@
+
+#ifStmt
+#if
+#if else
+# if elif elif elif elif ... else
+
+
+num = int(input("나이 입력 : "))
+money = 10000
+if num > 20:
+       money += 5000   #money = money + 5000
+
+print("당신의 입장료는",money,"입니다")
