d4c5928e02bc97cc74696bfd20c3b0a077d87690
jaehyuk-code <80086101+jaehyuk-code@users.noreply.github.com>
2021-05-07 15:48:07 +0900

    Add files via upload

---
+++ 0507/scope.py
@@ -0,0 +1,16 @@
+def func1(a):
+    a = a + 5
+    print(a)
+    return a
+def func2(i):
+    print(b)
+    print(i)
+
+
+a = int(input("정수 입력"))
+print(a)
+b = func1(a)
+print(a)
+print(b)
+func2(6)
+
